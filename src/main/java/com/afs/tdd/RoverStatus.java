971a8fa7104297608c4d8c7bead19ecbff499916
martier <martier@oocl.com>
2021-08-05 19:38:22 +0800

    [MarsRover][Eric] tests: add should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_0_directionN_and_M

---
+++ src/main/java/com/afs/tdd/RoverStatus.java
@@ -0,0 +1,17 @@
+package com.afs.tdd;
+
+public class RoverStatus {
+    private final int locationX;
+    private final int locationY;
+    private final String direction;
+
+    public RoverStatus(int locationX, int locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+}
---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,20 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MarsRoverTest {
+    @Test
+    void should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_0_directionN_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0,0,"N"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0,1,"N");
+
+        //when
+        marsRover.executeCommand("M");
+
+        //then
+        assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+    }
+}
