cb307c5fbe26045626380c38d4b21f0cc8ded499
martier <martier@oocl.com>
2021-08-05 20:49:41 +0800

    [MarsRover][Eric] feat: add implementation to should_return_locationX_0_locationY_0_direction_S_when_execute_command_given_locationX_0_locationY_0_direction_E_and_R

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -26,11 +26,21 @@ public class MarsRover {
 
         if (direction.equals("N")) {
             direction = "E";
+
+            roverStatus = new RoverStatus(locationX, locationY, direction);
+            return;
         }
         if (direction.equals("S")) {
             direction = "W";
+
+            roverStatus = new RoverStatus(locationX, locationY, direction);
+            return;
         }
-        roverStatus = new RoverStatus(locationX, locationY, direction);
+        if (direction.equals("E")) {
+            direction = "S";
+
+            roverStatus = new RoverStatus(locationX, locationY, direction);
+        }
     }
 
     private void turnLeft() {
